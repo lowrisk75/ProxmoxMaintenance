a1a2cfa1538d3cfc3a336bf126176042280e7a5e
Kevin N <37781161+lowrisk75@users.noreply.github.com>
2025-02-23 12:16:45 +0100

    Create config.template.sh

---
+++ config.template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Discord Webhook URL for notifications
+DISCORD_WEBHOOK=""
+
+# Backup configuration
+MOUNT_POINT="/mnt/pve/backup"
+MAX_BACKUPS=5
+
+# Other settings
+INTERNET_CHECK_TIMEOUT=5
